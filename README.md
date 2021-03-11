be93722ca7c3acc66e6ed8e5b86af09516d075df
Anto <m.antoberlianto1993@gmail.com>
2021-03-11 17:02:37 +0700

    update file README.md dan Script - Select Indonesia.sql
    1. Penambahan keterangan repository database indonesia
    2. Perubahan catatan query select

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Database Wilayah Indonesia
+Ini adalah database wilayah indonesia. \n
+Terdiri dari provinsi, kota, kecamatan, kelurahan dan kode pos.
 
-# Daftar Provinsi Indonesia
-Total Data Provinsi : 34
+# Provinsi Indonesia
+Total Provinsi : 34
+![Provinsi](https://github.com/muchanto1993/Database-Indonesia/blob/main/img/List%20Provinsi.png)
+
+# Kota di Indonesia
+Total Kota : 474
+
+# Kecamatan di Indonesia
+Total Kecamatan : 6.642
+
+# Kelurahan di Indonesia
+Total Kelurahan : 61.910
--- Script - Select Indonesia.sql
+++ Script - Select Indonesia.sql
@@ -1,18 +1,28 @@
-select * from t_province order by province_code asc;
-
+select * from t_province order by province_code ;
 select * from t_province_detail order by id asc;
 
+select count(*) from t_province;
+select count(*) from t_province_detail;
+
+select count(distinct city) from t_province_detail;
+select count(distinct sub_district) from t_province_detail;
+select count(distinct urban) from t_province_detail;
+
 select distinct(city) from t_province_detail order by city asc;
-select distinct(city) from t_province_detail tpc 
-	join t_province tp on tp.province_code = tpc.province_code 
-where upper(tp.province_name) = upper('jawa barat') order by city asc;
-
 select distinct(sub_district) from t_province_detail order by sub_district asc;
-select distinct(sub_district) from t_province_detail where upper(city) = upper('bandung') order by sub_district asc;
-
 select distinct(urban) from t_province_detail order by urban asc;
-select distinct(urban) from t_province_detail where upper(sub_district) = upper('kiaracondong') order by urban asc;
 
-select * from t_province_detail a 
-  join t_province b on a.province_code = b.province_code 
-where a.postal_code = '40283';
+select prov.province_code, dtl.id, dtl.city
+from t_province_detail dtl
+	join t_province prov on dtl.province_code = prov.province_code 
+where prov.province_name = 'JAWA BARAT'
+order by prov.province_code asc;
+
+select sub_district from t_province_detail where city = 'BANDUNG' order by id asc;
+select urban from t_province_detail where sub_district = 'KIARACONDONG' order by id asc;
+
+select prov.province_code, prov.province_name, prov.province_name_en,
+	dtl.city, dtl.sub_district, dtl.urban, dtl.postal_code
+from t_province_detail dtl
+  join t_province prov on dtl.province_code = prov.province_code 
+where dtl.postal_code = '40283';
